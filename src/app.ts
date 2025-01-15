749c249c7676653219ea5349e8953a940823da28
Eran <eranz126@gmail.com>
2025-01-15 23:36:51 +0200

    Add mongoose connection and error handling in connecting

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,35 @@
-const express = require('express');
+import dotenv from "dotenv"
+dotenv.config();
+import mongoose from "mongoose";
+import express, { Express } from "express";
+
 const app = express();
-const dotenv = require('dotenv').config();
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
+
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-});
+const initApp = () => {
+  return new Promise<Express>((resolve, reject) => {
+    if (!process.env.DB_CONNECT) {
+      reject("DB_CONNECT is not defined in .env file");
+    } else {
+      mongoose
+        .connect(process.env.DB_CONNECT)
+        .then(() => {
+          resolve(app);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    }
+  });
+};
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+initApp().then((app) => {
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
 });
